0befb225541bbdd0b705b8def8a794452fe902eb
0812728853 <145583955+0812728853@users.noreply.github.com>
2023-10-17 14:52:28 +0700

    Ex14-TuanAnh

---
+++ app.js
@@ -0,0 +1,72 @@
+// ex01
+// let input =  prompt("Nhap so");
+// function string(input) {
+// let arr = input.split(',');
+// let arr1=arr.sort();
+// let arr2= arr1.join(',');
+// return arr2;
+//  }
+//  let input1=string(input);
+//  console.log(input1);
+
+
+
+// // ex02
+// let input = prompt("Nhap chu");
+//  function string() {
+// let i=input.length;
+    
+//     console.log(i);
+//  }
+//  string(input);
+
+
+
+
+
+
+// // ex03
+// let input = prompt("Nhập vào một chuỗi");
+// function Upper(n) {
+//     max=n.toUpperCase();
+
+//     console.log(max);
+// } 
+// Upper(input);
+
+
+
+
+
+
+// // ex04
+// let input = prompt("Nhập vào một chuỗi");
+// let arr = input.split('');
+//  function string(n) {
+//    max=n.reverse();
+
+//     console.log(max);
+
+    
+//  }
+//  string(arr);
+ 
+
+// ex05
+let input=prompt("Nhap so");
+function string(input) {
+    let check=true;
+   for (let i = 0; i < input; i++) {
+    if (input[i]<2) {
+        check=false;
+    } else (input[i]%arr[i--]===0)
+    check=false;
+    break;
+   }
+    if (check) {
+        console.log(input);
+       
+    }
+        
+}
+string();
